ba0e2748010140d9f9f9a5f06a5df8569c3dacbf
Michal Bryxí <michal.bryxi@gmail.com>
2024-06-20 11:30:37 +0200

    chore: Cleanup old code

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,9 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  queryParams = {
-    // dateFrom: {},
-    // dateTo: {},
-    // highlightedPin: { replace: true },
-  };
+  queryParams = {};
 }
